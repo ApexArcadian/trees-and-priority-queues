6598d8cec43bb565b64fbdc06ac5c2fe34e7a788
ApexArcadian <85552345+ApexArcadian@users.noreply.github.com>
2026-04-21 16:07:38 -0400

    implementing base classes linked_binary_tree etc

---
+++ binary_tree.py
@@ -0,0 +1,36 @@
+
+from tree import Tree
+
+
+
+class BinaryTree(Tree):
+    """abstract class for binary tree structure"""
+    
+    #---additional abstract methods---#
+    def left(self, p):
+        """return a Position representing p's left child (or None if no left child)"""
+        raise NotImplementedError('must be implemented by subclass')
+    
+    def right(self, p):
+        """return a Position representing p's right child (or None if no right child)"""
+        raise NotImplementedError('must be implemented by subclass')
+    
+#-- concrete methods implemented in this class --#
+    def sibling(self, p):
+        """return a Position representing p's sibling (or None if no sibling)"""
+        parent = self.parent(p)
+        if parent is None:
+            return None
+        else:
+            if p == self.left(parent):
+                return self.right(parent)
+            else:
+                return self.left(parent)
+    def children(self, p):
+        """generate an iteration of Positions representing p's children"""
+        if self.left(p) is not None:
+            yield self.left(p)
+        if self.right(p) is not None:
+            yield self.right(p)
+            
+    #-- binary tree constructor
